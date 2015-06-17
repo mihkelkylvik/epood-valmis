ecc751df1b9bea2d0d546cfb59b7ecacc8dce137
mihkelkylvik <mihkel.kulvik@khk.ee>
2015-06-17 17:37:29 +0300

    DB

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
+mail("mihkel20@gmail.com", "Demo", "TERE YOLO.");//die();
 // Project constants
-define('PROJECT_NAME', 'halo');
+define('PROJECT_NAME', 'e_pood');
 define('DEFAULT_CONTROLLER', 'welcome');
 define('DEBUG', false);
 
